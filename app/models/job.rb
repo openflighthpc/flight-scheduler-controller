f5c35ddd431c5c7bfd6509877f9aa23da441406a
Ben Armston <ben.armston@alces-software.com>
2020-09-21 15:58:17 +0100

    Initial pass at modelling array job/tasks
    There are now three types of jobs: `JOB`, `ARRAY_JOB` and `ARRAY_TASK`.
    
    A `JOB` represents a non-array job, its submission script is submitted
    to a single node allocated to it.
    
    An `ARRAY_JOB` represents exactly that, an array job.  When created, it
    will have a number of `ARRAY_TASK`s created for it.  The array task is
    the job which is submitted to the daemon to be ran.  The state of the
    array task can be tracked separately from the array job.
    
    The scheduler does not concern itself with `ARRAY_TASK`, instead it
    allocates resources and schedules `JOB`s and `ARRAY_JOB`s.  When an
    `ARRAY_JOB` is ran its `ARRAY_TASK`s will be submitted to the allocated
    nodes.
    
    Limitations of array tasks:
    
    Currently, only a single node allocated to an array job is used to run
    the array tasks, and the tasks are not ran in parallel.  To fix these,
    we need some way of tracking how many tasks can be ran on per-node and
    which node has been allocated to which task.  Our allocation and job
    models do not currently support.

--- app/models/job.rb
+++ app/models/job.rb
@@ -33,21 +33,42 @@ class Job
     attr_reader :job_dir
   end
 
-  STATES = %w( PENDING RUNNING CANCELLED COMPLETED FAILED )
+  STATES = %w( PENDING RUNNING CANCELLED COMPLETED FAILED ).freeze
   STATES.each do |s|
     define_method("#{s.downcase}?") { self.state == s }
   end
+  def completed?
+    job_type == 'ARRAY_JOB' ?
+      array_tasks.all?(&:completed?) :
+      self.state == 'COMPLETED'
+  end
 
-  attr_writer :arguments
+  JOB_TYPES = %w( JOB ARRAY_JOB ARRAY_TASK ).freeze
+
+  # The index of the task inside the array job.  Only present for ARRAY_TASKS.
+  attr_accessor :array_index
+
+  # A reference to the ARRAY_JOB.  Only present for ARRAY_TASKS.
+  attr_accessor :array_job
+
+  # A reference to all of an ARRAY_JOB's ARRAY_TASKS.  Only present for
+  # ARRAY_JOBS.
+  attr_accessor :array_tasks
+
   attr_accessor :id
+  attr_accessor :job_type
   attr_accessor :partition
-  attr_accessor :state
   attr_accessor :script_name
   attr_accessor :script_provided
+  attr_accessor :state
+  attr_writer :arguments
 
-  # Handle the k and m suffix
+  # A list of array indexes.  Only present for ARRAY_JOBS.
+  attr_writer :array
+
   attr_reader :min_nodes
 
+  # Handle the k and m suffix
   def min_nodes=(raw)
     str = raw.to_s
     @min_nodes = if /\A\d+k\Z/.match?(str)
@@ -62,15 +83,35 @@ class Job
     end
   end
 
-  validates :script_name, presence: true
-  validates :script_provided, inclusion: { in: [true] }
   validates :id, presence: true
   validates :min_nodes,
     presence: true,
     numericality: { allow_blank: false, only_integer: true, greater_than_or_equal_to: 1 }
+  validates :script_name, presence: true
+  validates :script_provided, inclusion: { in: [true] }
   validates :state,
     presence: true,
     inclusion: { within: STATES }
+  validates :job_type,
+    presence: true,
+    inclusion: { within: JOB_TYPES }
+
+  # Validations for ARRAY_JOBS
+  validate :validate_array_tasks!, if: ->() { job_type == 'ARRAY_JOB' }
+
+  # Validations for ARRAY_TASKS
+  validates :array_index,
+    presence: true,
+    numericality: { allow_blank: false, only_integer: true, greater_than_or_equal_to: 0 },
+    if: ->() { job_type == 'ARRAY_TASK' }
+  validates :array_job,
+    presence: true, if: ->() { job_type == 'ARRAY_TASK' }
+
+  # Validations for non-ARRAY_TASKS
+  validates :array_index,
+    absence: true, unless: ->() { job_type == 'ARRAY_TASK' }
+  validates :array_job,
+    absence: true, unless: ->() { job_type == 'ARRAY_TASK' }
 
   # Must be called at the end of the job lifecycle to remove the script
   def cleanup
@@ -91,7 +132,21 @@ class Job
   end
 
   def allocation
-    FlightScheduler.app.allocations.for_job(self.id)
+    job_id = job_type == 'ARRAY_TASK' ? array_job.id : self.id
+    FlightScheduler.app.allocations.for_job(job_id)
+  end
+
+  def create_array_tasks
+    self.array_tasks ||= @array.split(',').map do |idx|
+      self.dup.tap do |task|
+        task.array_index = idx
+        task.array_job = self
+        task.id = SecureRandom.uuid
+        task.job_type = 'ARRAY_TASK'
+        task.state = 'PENDING'
+        task.validate!
+      end
+    end
   end
 
   def arguments
@@ -100,5 +155,11 @@ class Job
 
   def hash
     [self.class, id].hash
+  end
+
+  def validate_array_tasks!
+    array_tasks.each do |task|
+      task.validate!
+    end
   end
 end
