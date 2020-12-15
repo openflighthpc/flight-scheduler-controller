94c1a77bf5fea7533fabe5d8fd4e6c10cca43855
William McCumsite <william.mccumstie@gmail.com>
2020-12-15 15:38:51 +0000

    Validate the entire parition schema

--- app/models/partition.rb
+++ app/models/partition.rb
@@ -26,6 +26,42 @@
 #==============================================================================
 
 class Partition
+  PARTITION_SCHEMA = {
+    "type" => "object",
+    "additionalProperties" => false,
+    "required" => ["name"],
+    "properties" => {
+      "name" => { "type" => 'string' },
+      "default" => { "type" => 'boolean' },
+      "nodes" => { "type" => "array", "items" => { "type" => "string" } },
+      "max_time_limit" => { "type" => ['string', 'integer'] },
+      "default_time_limit" => { "type" => ['string', 'integer'] },
+      "node_matchers" => { "type" => "array", "items" => FlightScheduler::NodeMatcher::SCHEMA }
+    }
+  }
+  ROOT_SCHEMA = {
+    "type" => "object",
+    "additionalProperties" => false,
+    "properties" => {
+      "partitions" => {
+        "type" => "array",
+        "items" => PARTITION_SCHEMA
+      }
+    }
+  }
+
+  VALIDATOR = JSONSchemer.schema(ROOT_SCHEMA)
+
+  Builder = Struct.new(:specs) do
+    def valid?
+      errors.empty?
+    end
+
+    def errors
+      @errors ||= VALIDATOR.validate({ "partitions" => specs }).to_a
+    end
+  end
+
   attr_reader :name, :nodes, :max_time_limit, :default_time_limit
 
   def initialize(
