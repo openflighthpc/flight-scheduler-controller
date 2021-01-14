c3bc034ca89936fccb8233ac409f461c902a7056
William McCumsite <william.mccumstie@gmail.com>
2021-01-14 13:21:08 +0000

    Flag jobs as CONFIGURING after they are allocated

--- lib/flight_scheduler/event_processor.rb
+++ lib/flight_scheduler/event_processor.rb
@@ -89,6 +89,13 @@ module FlightScheduler::EventProcessor
       Async.logger.info("Allocated #{allocated_node_names} to job #{allocation.job.display_id}")
       FlightScheduler::Submission::Job.new(allocation).call
     end
+
+    # The allocation/job registries are coupled due to the job's state. A job that is in the
+    # CONFIGURING or RUNNING state must have an allocation and vice-versa
+    #
+    # Saving the two registries independently means this update can not be done in a transaction.
+    # Considering refactoring to save all registries as single file, or infer the state via some
+    # other means.
     FlightScheduler.app.job_registry.save
     FlightScheduler.app.allocations.save
   end
@@ -207,7 +214,7 @@ module FlightScheduler::EventProcessor
       Async.logger.info("Cancelling pending job #{job.display_id}")
       job.state = 'CANCELLED'
       allocate_resources_and_run_jobs
-    when 'RUNNING', 'CANCELLING'
+    when 'CONFIGURING', 'RUNNING', 'CANCELLING'
       Async.logger.info("Cancelling running job #{job.display_id}")
       FlightScheduler::Cancellation::Job.new(job).call
     else
