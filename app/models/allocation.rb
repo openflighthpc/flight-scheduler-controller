openflighthpc/flight-scheduler-controller

commit 494a280274c90fd44ed8912ee9322fbfc5e1ecc5

Simplify conditional

lib/flight_scheduler/job_registry.rb
@@ -70,7 +70,7 @@ class FlightScheduler::JobRegistry
         begin
           # Its possible that we're still processing the request to deallocate
           # the job.  If that's the case, we'll clean this job up another time.
-          if job.allocated? || FlightScheduler.app.job_registry.tasks_for(job).any?(&:allocated?)
+          if job.allocated?
             Async.logger.debug("Skipping job:#{job.display_id} due to existing allocation")
           else
             delete(job)

app/models/job.rb
@@ -264,8 +264,14 @@ class Job
     end
   end
 
-  def allocated?
-    allocation ? true : false
+  def allocated?(include_tasks: false)
+    if allocation
+      true
+    elsif include_tasks && job_type == 'ARRAY_JOB'
+      FlightScheduler.app.job_registry.tasks_for(self).any?(&:allocated?)
+    else
+      false
+    end
   end
 
   def allocation

spec/models/job_spec.rb
@@ -31,6 +31,55 @@ RSpec.describe Job, type: :model do
     expect(build(:job)).to be_valid
   end
 
+  describe '#allocated?' do
+    context 'when the job is an array job' do
+      let(:job) { build(:job, array: '1,2') }
+
+      it 'returns true if there is an allocation' do
+        allow(job).to receive(:allocation).and_return(Object.new)
+        expect(job.allocated?).to be true
+      end
+
+      it 'returns false if there is not an allocation' do
+        allow(job).to receive(:allocation).and_return(nil)
+        expect(job.allocated?).to be false
+        expect(job.allocated?(include_tasks: true)).to be false
+      end
+
+      it 'returns true if a task has an allocation and tasks are interrogated' do
+        task = job.task_generator.next_task
+        allow(job).to receive(:allocation).and_return(nil)
+        allow(task).to receive(:allocation).and_return(Object.new)
+        allow(FlightScheduler.app.job_registry).to receive(:tasks_for).and_return([task])
+
+        expect(job.allocated?(include_tasks: true)).to be true
+        expect(job.allocated?(include_tasks: false)).to be false
+      end
+    end
+
+    context 'when the job is not array job' do
+      let(:job) { build(:job) }
+
+      it 'returns true if there is an allocation' do
+        allow(job).to receive(:allocation).and_return(Object.new)
+        expect(job.allocated?).to be true
+      end
+
+      it 'returns false if there is not an allocation' do
+        allow(job).to receive(:allocation).and_return(nil)
+        expect(job.allocated?).to be false
+      end
+
+      it 'returns false if tasks are interrogated' do
+        allow(job).to receive(:allocation).and_return(nil)
+        allow(FlightScheduler.app.job_registry).to receive(:tasks_for).and_return([])
+
+        expect(job.allocated?(include_tasks: true)).to be false
+        expect(job.allocated?(include_tasks: false)).to be false
+      end
+    end
+  end
+
   describe '#min_nodes' do
     # Ensure the min nodes is overridden
     let(:input_min_nodes) { raise NotImplementedError }

commit ba73a4f3242e3947f7f929ca61ad694b8dc6e235

Fix deallocation race condition
Introduced in 494a280, cleaning up completed ARRAY_JOBS didn't check
that its ARRAY_TASKs had been deallocated.  This resulted in the job
being removed whilst the allocation continued to live on.

This commit fixes the issue by checking ARRAY_TASKs.

We should also consider how to deal with de-allocations when the job is
not known.

app/models/allocation.rb
@@ -103,4 +103,8 @@ class Allocation
       node_names: @node_names,
     }
   end
+
+  def debug
+    "#{job.debug_id}:#{@node_names}"
+  end
 end

lib/flight_scheduler/allocation_registry.rb
@@ -101,8 +101,7 @@ class FlightScheduler::AllocationRegistry
       @job_allocations[allocation.job.id] = allocation
     end
 
-    allocated_node_names = allocation.nodes.map(&:name).join(',')
-    Async.logger.info("[allocation registry] added #{allocation.job.debug_id}:#{allocated_node_names}")
+    Async.logger.info("[allocation registry] added #{allocation.debug}")
   end
 
   # NOTE: This method currently removes all instances of the node from the allocation
@@ -117,7 +116,7 @@ class FlightScheduler::AllocationRegistry
 
       # Remove the node from the allocation
       allocation.remove_node(node_name)
-      Async.logger.info("[allocation registry] removed #{node_name} from #{allocation.job.debug_id}")
+      Async.logger.info("[allocation registry] removed #{node_name} from #{allocation.debug}")
       @node_allocations[node_name].delete(allocation)
 
       # Remove empty job allocations if required

lib/flight_scheduler/job_registry.rb
@@ -72,9 +72,11 @@ class FlightScheduler::JobRegistry
         begin
           # Its possible that we're still processing the request to deallocate
           # the job.  If that's the case, we'll clean this job up another time.
-          if job.allocated?
-            Async.logger.debug("[job registry] skipping job:#{job.debug_id}") {
-              job.allocation
+          if job.allocated?(include_tasks: true)
+            Async.logger.info("[job registry] skipping job:#{job.debug_id} has allocations")
+            Async.logger.debug("[job registry] allocations for job:#{job.debug_id}") {
+              tasks = FlightScheduler.app.job_registry.tasks_for(job)
+              [ job.allocation, tasks.map(&:allocation) ].flatten.compact.map(&:debug)
             }
           else
             delete(job)
@@ -158,7 +160,11 @@ class FlightScheduler::JobRegistry
 
     @lock.with_write_lock do
       if job.job_type == 'ARRAY_JOB'
-        @tasks[job.id].each { |job| job.cleanup }
+        if @tasks[job.id].nil?
+          Async.logger.error("[job registry] unable to find tasks for array job#{job.debug_id}")
+        else
+          @tasks[job.id].each { |job| job.cleanup }
+        end
         @jobs.delete_if { |id, j| j.array_job == job }
         @tasks.delete(job.id)
       end

lib/flight_scheduler/plugins/core/domain_model.rb
@@ -173,6 +173,7 @@ class DomainModel
         job.state = 'CANCELLED'
       else
         Async.logger.info("[core] cancelling running array tasks for #{job.display_id}")
+        Async.logger.debug("[core] running tasks for job:#{job.display_id} #{job.running_tasks.map(&:debug_id)}")
         FlightScheduler::Cancellation::ArrayJob.new(job).call
       end
     end